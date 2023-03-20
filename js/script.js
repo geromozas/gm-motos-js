254a580b2a6c4b0f283378f6c168c12292e71df4
Gerónimo Mozas <mozasgeronimo@gmail.com>
2023-03-20 11:32:49 -0300

    2da pre entrega

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-let modelos = ""
+const motos = [
+    {id: 1, marca: "kawasaki", modelo: "Z400", precio: 11100},
+    {id: 2, marca: "kawasaki", modelo: "Ninja 400", precio: 13350},
+    {id: 3, marca: "kawasaki", modelo: "Versys 300", precio: 13250},
+    {id: 4, marca: "yamaha", modelo: "FZ25", precio: 3700},
+    {id: 5, marca: "yamaha", modelo: "MT03", precio: 12200},
+    {id: 6, marca: "yamaha", modelo: "R3", precio: 12900},
+    {id: 7, marca: "honda", modelo: "CB 250 twister", precio: 3500},
+    {id: 8, marca: "honda", modelo: "CB500F", precio: 12900},
+    {id: 9, marca: "honda", modelo: "CB500X", precio: 14500},
+    {id: 10, marca: "ktm", modelo: "Duke 200 ng", precio: 4500},
+    {id: 11, marca: "ktm", modelo: "Duke 390", precio: 11100},
+    {id: 12, marca: "ktm", modelo: "RC390", precio: 11500}] 
+
 let continuar = true
 
 function ingreso (){
@@ -22,29 +35,16 @@ const marcas = "Ingresa la marca de motos que desea consultar: \n"+
 function iniciarConsulta(){
     let seleccion = prompt(marcas).toLowerCase().trim()
     
-    if (seleccion !== "a" && seleccion !== "b" && seleccion !== "c" && seleccion !== "d" && seleccion !== "e"){
+    if (seleccion !== "kawasaki" && seleccion !== "yamaha" && seleccion !== "honda" && seleccion !== "benelli" && seleccion !== "ktm"){
         alert("Debes ingresar un codigo válido, por favor.")
         return
     }else {
-        switch(seleccion){
-            case "a":
-                modelos = "Tenemos disponibles los siguientes modelos:\n - Z400\n - Ninja 400\n - Versys 300"
-                break;
-            case "b":
-                modelos = "Tenemos disponibles los siguientes modelos:\n - FZ25\n - MT03\n - R3"
-                break;
-            case "c":
-                modelos = "Tenemos disponibles los siguientes modelos:\n - CB 250 twister\n - CB 500F\n - CB 500X"
-                break;
-            case "d":
-                modelos = "Tenemos disponibles los siguientes modelos:\n - Leoncino 250\n - 302S\n - 752S"
-                break;
-            case "e":
-                modelos = "Tenemos disponibles los siguientes modelos:\n - Duke 200\n - Duke 390\n - RC390"
-                break;
-            default:
-                console.error("Algo se rompio. No deberia ver este mensaje");
+        let motosFiltrado = motos.filter(m => m.marca === seleccion)
+        let nombres = [];
+        for(i = 0; i<motosFiltrado.length; i++){
+            nombres.push(motosFiltrado[i].modelo)
         }
+        let modelos = `Tenemos disponibles los siguientes modelos\n ${nombres}`
         alert(modelos);
     }
 }
